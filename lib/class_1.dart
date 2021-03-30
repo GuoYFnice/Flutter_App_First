ecd55e55a10cde8383ab2c3320af56d4baa9c1d1
e <603776827@qq.com>
2021-03-30 10:58:55 +0800

    feat:修改一下项目的格式（展示多个练习形式）

---
+++ lib/class_1.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+/*
+* class_1
+* 一个简单的flutter布局练习‘
+*/
+
+// 声明MyApp类
+class MyApp extends StatelessWidget {
+  //重写build方法
+  @override
+  Widget build(BuildContext context) {
+    //返回一个Material风格的组件
+    return MaterialApp(
+      title: 'Welcome to Flutteraa',
+      home: Scaffold(
+        //创建一个Bar，并添加文本
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        //在主体的中间区域，添加一个hello world 的文本
+        body: Center(
+          child: Container(child: MyList()),
+        ),
+      ),
+    );
+  }
+}
+
+class MyList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      // scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        new Container(
+          width: 180.0,
+          height: 390.0,
+          color: Colors.lightBlue,
+          child: Text(
+            'TEST 1',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 25.0,
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.solid,
+            ),
+          ),
+        ),
+        new Container(
+          width: 180.0,
+          height: 390.0,
+          color: Colors.amber,
+          child: Text(
+            'TEST 2',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 25.0,
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.solid,
+            ),
+          ),
+        ),
+        new Container(
+          width: 180.0,
+          height: 390.0,
+          color: Colors.deepOrange,
+          child: Text(
+            'TEST 3',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 25.0,
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.solid,
+            ),
+          ),
+        ),
+        new Container(
+          width: 180.0,
+          height: 390.0,
+          color: Colors.deepPurpleAccent,
+          child: Text(
+            'TEST 4',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 25.0,
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.solid,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/class_2.dart
@@ -0,0 +1,68 @@
+// 导入包。
+import 'package:flutter/material.dart';
+/*
+* class_2
+* 路由相关
+*/
+
+// lutter在构建页面时，会调用组件的build方法，widget的主要工作是提供一个build()方法来描述如何构建UI界面（通常是通过组合、拼装其它基础widget）。
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+// MyHomePage 是Flutter应用的首页，它继承自StatefulWidget类，表示它是一个有状态的组件（Stateful widget）。
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold 是 Material 库中提供的页面脚手架，它提供了默认的导航栏、标题和包含主屏幕widget树（后同“组件树”或“部件树”）的body属性，组件树可以很复杂。本书后面示例中，路由默认都是通过Scaffold创建。
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'You have pushed the button this many times:',
+            ),
+            new Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
